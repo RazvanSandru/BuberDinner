ab2e2baf0c5af302eaf59c837426211daf53f727
Razvan Sandru <razvansandru93@gmail.com>
2022-12-12 10:33:03 +0200

    Created Menu Aggregate and adjacent value objects

---
+++ BuberDinner.Domain/Common/ValueObjects/Rating.cs
@@ -0,0 +1,22 @@
+﻿using BuberDinner.Domain.Common.Models;
+
+namespace BuberDinner.Domain.Common.ValueObjects;
+public class Rating : ValueObject
+{
+	public double Value { get; private set; }
+
+	private Rating(double value)
+	{
+		Value = value;
+	}
+
+	public static Rating CreateNew(double rating = 0)
+	{
+		return new Rating(rating);
+	}
+
+	public override IEnumerable<object> GetEqualityComponents()
+	{
+		yield return Value;
+	}
+}
